bb40cae6892ac688a7e9fcb65379b3f0896f66d6
Zaid Abdulla <zaid.abdulla@auis.org>
2011-01-12 16:41:53 +0300

    Displaying peer status in inbox page.

--- peerpalette.com/InboxPage.py
+++ peerpalette.com/InboxPage.py
@@ -3,6 +3,7 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 from google.appengine.ext import db
 
+import config
 import models
 import common
 import os
@@ -13,17 +14,25 @@ import datetime
 class InboxPage(webapp.RequestHandler):
   def get(self):
     user = common.get_user()
-    convs = db.Query(models.UserChat).filter('user =', user).order('-last_updated')
+    convs = db.Query(models.UserChat).filter('user =', user).order('-last_updated').fetch(config.ITEMS_PER_PAGE);
     
     conversations = []
+
+    peer_keys = [conv.peer.key() for conv in convs]
+    peers = dict(zip(peer_keys, db.get(peer_keys)))
+
     for conv in convs:
       if not conv.last_updated:
         continue
+
+      status = common.get_user_status(peers[conv.peer.key()])
+      status_class = common.get_status_class(status)
+
       try:
         i = user.unread_chat.index(conv.key())
-        conversations.append({"title" : conv.title, "id" : conv.key().id(), "read" : False})
+        conversations.append({"title" : conv.title, "id" : conv.key().id(), "read" : False, "status_class" : status_class})
       except:
-        conversations.append({"title" : conv.title, "id" : conv.key().id(), "read" : True})
+        conversations.append({"title" : conv.title, "id" : conv.key().id(), "read" : True, "status_class" : status_class})
 
     template_values = {
       "conversations" : conversations,
